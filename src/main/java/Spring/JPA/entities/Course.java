719f3d07d3132e532aab17993995e36202045b7e
Mwawaka <kedskeds90@gmail.com>
2023-02-01 16:04:23 +0300

    Update:Course entity

---
+++ src/main/java/Spring/JPA/entities/Course.java
@@ -0,0 +1,23 @@
+package Spring.JPA.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+
+    @Id
+    @SequenceGenerator(name="course_sequence",sequenceName = "course_sequence")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE,generator = "course_sequence")
+    private Long courseId;
+    private String title;
+
+    private Integer credit;
+}
